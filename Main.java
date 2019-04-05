34ca6ddb57e9fa88b25024b4b5c94e4dd74a3086
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-04-05 13:20:38 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+//Andrea Paniagua, Diego Solorzano
+//18733,
+//Este es el main en el que el usuario ingresa a los pacientesx
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     private static VectorHeap<Patient> heapPrincipal = new VectorHeap<>();
     public static void main(String[] args){
+        try{
+            FileReader fr = new FileReader("Pacientes.txt");
+            
+        }catch (FileNotFoundException e){
+            System.out.println("No se encontró el archivo");
+        }catch (IOException e){
+
+        }
         //read File here
         //patient newPatient = new Patient(nombreNuevo, condicionNueva, gravedadNueva)
         //VectorHeap.add(newPatient)
