f1fa56215046f0f5154dcbbe9b01fcabcc5cb182
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-04-05 21:59:26 -0600

    Update MainVH.java

--- MainVH.java
+++ MainVH.java
@@ -1,25 +1,40 @@
-//Andrea Paniagua, Diego Solorzano
-//18733,
-//Este es el main en el que el usuario ingresa a los pacientesx
+//Andrea Paniagua, Juan Diego Solorzano
+//18733, 18151
+//Este es el main en el que el usuario ingresa a los pacientes
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class MainVH {
     private static VectorHeap<Patient> heapPrincipal = new VectorHeap<>();
-    public static void main(String[] args){
-        PriorityQueue<Patient> preQueue = new PriorityQueue<>();
-        try{
+
+    public static void main(String[] args) {
+        try {
             FileReader fr = new FileReader("Pacientes.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String text;
+            int i = 0;
+            while ((text = br.readLine()) != null) {
+                String[] partes = text.split(", ");
+                String nombre = partes[0];
+                String descripcion = partes[1];
+                String gravedad = partes[2];
+                heapPrincipal.add(new Patient(nombre, descripcion, gravedad));
 
-        }catch (FileNotFoundException e){
+            }
+
+
+        } catch (FileNotFoundException e) {
             System.out.println("No se encontró el archivo");
-        }catch (IOException e){
+        } catch (IOException e) {
 
         }
-        //VectorHeap.add(newPatient) esto es en el main del vector heap
+
+        System.out.println("Listado de pacientes (nombre, condicion, prioridad):");
         while (!heapPrincipal.isEmpty()) {
-            System.out.println(heapPrincipal.remove().nombre);
+            System.out.println(heapPrincipal.getFirst().nombre + ", " + heapPrincipal.getFirst().condicion + ", " + heapPrincipal.remove().gravedad);
         }
+
     }
 }
