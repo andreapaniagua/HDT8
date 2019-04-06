ef463e8f3010cad220d99ce6fe3bf13c5d5358fa
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-04-05 22:27:50 -0600

    Update MainVH.java

--- MainVH.java
+++ MainVH.java
@@ -1,6 +1,6 @@
 //Andrea Paniagua, Juan Diego Solorzano
 //18733, 18151
-//Este es el main en el que el usuario ingresa a los pacientes
+//Este es el main en el que se implementa el Priority Queue con Vector Heap
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
