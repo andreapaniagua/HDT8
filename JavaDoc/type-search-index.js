74cbfeb6dabe96e5df1a2b722f1cd050cf3b0e74
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-04-05 22:30:27 -0600

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"sample","l":"Controller"},{"p":"sample","l":"Main"},{"p":"sample","l":"MainVH"},{"p":"sample","l":"Patient"},{"p":"sample","l":"PriorityQueue"},{"p":"sample","l":"VectorHeap"}]
