4b145f03235b0b9ac196ada52bf38270a77a5c99
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-04-05 22:22:18 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,41 @@
-//Andrea Paniagua, Diego Solorzano
-//18733,
-//Este es el main en el que el usuario ingresa a los pacientesx
+//Andrea Paniagua, Juan Diego Solorzano
+//18733, 18151
+//Este es el main en el que el usuario ingresa a los pacientes
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.PriorityQueue;
 
 public class Main {
-    private static VectorHeap<Patient> heapPrincipal = new VectorHeap<>();
+
     public static void main(String[] args){
+
+        PriorityQueue<Patient> queuePrincipal = new PriorityQueue<Patient>();
         try{
             FileReader fr = new FileReader("Pacientes.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String text;
+            int i = 0;
+            while((text = br.readLine()) != null){
+                String[] partes = text.split(", ");
+                String nombre = partes[0];
+                String descripcion = partes[1];
+                String gravedad = partes[2];
+                queuePrincipal.add(new Patient(nombre, descripcion, gravedad));
+
+            }
+
 
         }catch (FileNotFoundException e){
             System.out.println("No se encontró el archivo");
         }catch (IOException e){
 
         }
-        //read File here
-        //patient newPatient = new Patient(nombreNuevo, condicionNueva, gravedadNueva)
-        //VectorHeap.add(newPatient) esto es en el main del vector heap
-        while (!heapPrincipal.isEmpty()) {
-            System.out.println(heapPrincipal.remove().nombre);
+
+        System.out.println("Listado de pacientes utilizando Java Collection Framework(nombre, condicion, prioridad):");
+        while (!queuePrincipal.isEmpty()) {
+            System.out.println(queuePrincipal.peek().nombre + ", " + queuePrincipal.peek().condicion + ", " + queuePrincipal.remove().gravedad);
         }
     }
 }
